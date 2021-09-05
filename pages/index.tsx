1e52add8ed8dc614f1e450ffe227ee35d3d8407d
Marius Florescu <mariusflorescu23@icloud.com>
2021-09-05 17:59:31 +0300

    Added _document

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,9 @@ import { useTheme } from 'next-themes'
 const Home: NextPage = () => {
   const {theme, setTheme} = useTheme();
   return (
-    <div className='bg-green-500'>
-      Hello
+    <div>
       <button
-          className="mt-16 px-4 py-2 text-white dark:text-black bg-black dark:bg-white font-semibold rounded-md"
+          className="m-16 px-4 py-2 text-white dark:text-black bg-black dark:bg-white font-semibold rounded-md"
           onClick={() => {
             setTheme(theme === 'light' ? 'dark' : 'light')
           }}
