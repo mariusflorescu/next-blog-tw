5653512a4627b5fcea2101b6b125b64a91440985
Marius Florescu <mariusflorescu23@icloud.com>
2021-09-05 17:51:15 +0300

    Added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+      './components/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
