41a291009ebfb916d10d3c0ce5105b9babe1b9c7
Marius Florescu <mariusflorescu23@icloud.com>
2021-09-11 01:03:43 +0300

    Fixed color issue on arrow (pretty dumb that it doesnt work from classed but whatever)

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useTheme} from "next-themes";
 import * as TooltipPrimitive from '@radix-ui/react-tooltip'
 
 interface ITooltip {
@@ -11,14 +12,17 @@ interface ITooltip {
 }
 
 const Tooltip : React.FC<ITooltip> = ({children,side ,text}) => {
+    const [open,setOpen] = useState(false);
+    const {theme} = useTheme()
+
     return (
-        <TooltipPrimitive.Root delayDuration={300}>
+        <TooltipPrimitive.Root delayDuration={300} open={open} onOpenChange={(e) => setOpen(e)}>
             <TooltipPrimitive.Trigger asChild>
                 {children}
             </TooltipPrimitive.Trigger>
             <TooltipPrimitive.Content side={side} align="center" sideOffset={4} className='bg-gray-100 dark:bg-gray-800 p-2 rounded'>
                 {text}
-                <TooltipPrimitive.Arrow className='fill-current text-gray-100 dark:text-gray-800'/>
+                <TooltipPrimitive.Arrow fill={theme === 'light' ? '#F5F5F5' : '#262626'}/>
             </TooltipPrimitive.Content>
         </TooltipPrimitive.Root>
     )
