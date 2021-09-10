c9fb99db3b1f2159b85d88d87d12a739fe887b28
Marius Florescu <mariusflorescu23@icloud.com>
2021-09-11 01:04:45 +0300

    sideoffset + refactor

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -12,15 +12,15 @@ interface ITooltip {
 }
 
 const Tooltip : React.FC<ITooltip> = ({children,side ,text}) => {
-    const [open,setOpen] = useState(false);
     const {theme} = useTheme()
+    const [open,setOpen] = useState(false);
 
     return (
         <TooltipPrimitive.Root delayDuration={300} open={open} onOpenChange={(e) => setOpen(e)}>
             <TooltipPrimitive.Trigger asChild>
                 {children}
             </TooltipPrimitive.Trigger>
-            <TooltipPrimitive.Content side={side} align="center" sideOffset={4} className='bg-gray-100 dark:bg-gray-800 p-2 rounded'>
+            <TooltipPrimitive.Content side={side} align="center" sideOffset={5} className='bg-gray-100 dark:bg-gray-800 p-2 rounded'>
                 {text}
                 <TooltipPrimitive.Arrow fill={theme === 'light' ? '#F5F5F5' : '#262626'}/>
             </TooltipPrimitive.Content>
